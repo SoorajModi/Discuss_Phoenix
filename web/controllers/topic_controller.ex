73c86d3167e341b7022ea23a28fdbb780c0d5cba
sooraj.modi <sooraj.modi@bell.ca>
2019-06-22 17:56:06 -0400

    Generate new changeset to use whenever we show a form to the user to make a topic

--- web/controllers/topic_controller.ex
+++ web/controllers/topic_controller.ex
@@ -1,8 +1,11 @@
 defmodule Discuss.TopicController do
   use Discuss.Web, :controller
 
+  alias Discuss.Topic
+
   # conn = connection, Elixir struct representing both the incoming and outgoing request
   # params = to help us parse the URL
   def new(conn, params) do
+    changeset = Topic.changeset(%Topic{},  %{})
   end
 end
