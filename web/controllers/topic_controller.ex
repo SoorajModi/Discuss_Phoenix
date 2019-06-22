81a3754ccc53c3fed999786a56ee9f160075e1c8
sooraj.modi <sooraj.modi@bell.ca>
2019-06-22 17:47:46 -0400

    Add validation for changeset

---
+++ web/controllers/topic_controller.ex
@@ -0,0 +1,8 @@
+defmodule Discuss.TopicController do
+  use Discuss.Web, :controller
+
+  # conn = connection, Elixir struct representing both the incoming and outgoing request
+  # params = to help us parse the URL
+  def new(conn, params) do
+  end
+end
--- web/models/topic.ex
+++ web/models/topic.ex
@@ -4,4 +4,12 @@ defmodule Discuss.Topic do
   schema "topics" do
     field :title, :string
   end
+
+  # struct represents a record in our database
+  # a description of the new properties we want to update our struct with
+  def changeset(struct, params \\ %{}) do
+    struct
+    |> cast(params, [:title]) # produces a changeset (how we want to update the database)
+    |> validate_required([:title])  # inspect properties of the changeset and determines if its valid or not
+  end
 end
