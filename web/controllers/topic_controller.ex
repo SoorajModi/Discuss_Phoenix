f2645bed171a7b3e0f9e08e244b4d9e090bea47d
sooraj.modi <sooraj.modi@bell.ca>
2019-06-22 18:02:40 -0400

    Add html form to return

--- web/controllers/topic_controller.ex
+++ web/controllers/topic_controller.ex
@@ -7,5 +7,7 @@ defmodule Discuss.TopicController do
   # params = to help us parse the URL
   def new(conn, params) do
     changeset = Topic.changeset(%Topic{},  %{})
+
+    render conn, "new.html"
   end
 end
